0b8cfa589162de2598f4694f15a71de173cc60b2
peterwgood <peterwgood@gmail.com>
2024-08-08 14:25:36 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,120 +1,25 @@
-class Nav {
-    constructor(container) {
-        this.container = container;
-        this.createNav();
-    }
-
-    createNav() {
-        const nav = document.createElement('nav');
-        nav.className = 'navbar bg-dark border-bottom border-body';
-        nav.dataset.bsTheme = 'dark';
-
-        const containerFluid = document.createElement('div');
-        containerFluid.className = 'container-fluid';
-
-        const brand = document.createElement('a');
-        brand.className = 'navbar-brand';
-        brand.href = 'index.html';
-        brand.textContent = 'WT';
-
-        const toggler = document.createElement('button');
-        toggler.className = 'navbar-toggler';
-        toggler.type = 'button';
-        toggler.dataset.bsToggle = 'offcanvas';
-        toggler.dataset.bsTarget = '#offcanvasNavbar';
-        toggler.ariaControls = 'offcanvasNavbar';
-        toggler.ariaLabel = 'Toggle navigation';
-
-        const togglerIcon = document.createElement('span');
-        togglerIcon.className = 'navbar-toggler-icon';
-        toggler.appendChild(togglerIcon);
-
-        const offcanvas = document.createElement('div');
-        offcanvas.className = 'offcanvas offcanvas-end';
-        offcanvas.tabIndex = '-1';
-        offcanvas.id = 'offcanvasNavbar';
-
-        const offcanvasHeader = document.createElement('div');
-        offcanvasHeader.className = 'offcanvas-header';
-
-        const offcanvasTitle = document.createElement('h5');
-        offcanvasTitle.className = 'offcanvas-title';
-        offcanvasTitle.id = 'offcanvasNavbarLabel';
-        offcanvasTitle.textContent = 'WT';
-
-        const closeBtn = document.createElement('button');
-        closeBtn.type = 'button';
-        closeBtn.className = 'btn-close';
-        closeBtn.dataset.bsDismiss = 'offcanvas';
-        closeBtn.ariaLabel = 'Close';
-
-        const offcanvasBody = document.createElement('div');
-        offcanvasBody.className = 'offcanvas-body';
-
-        const navList = document.createElement('ul');
-        navList.className = 'navbar-nav justify-content-end flex-grow-1 pe-3';
-
-        const navItems = [{
-                text: 'Day',
-                href: 'index.html'
-            },
-            {
-                text: 'Week',
-                href: 'week.html'
-            },
-            {
-                text: 'Weekly Totals',
-                href: 'yearly.html'
-            },
-            {
-                text: 'Weight',
-                href: 'weight.html'
-            },
-            {
-                text: 'Energy',
-                href: 'Boosters.html'
-            },
-            {
-                text: 'To-do',
-                href: 'list.html'
-            },
-            {
-                text: 'Groceries',
-                href: 'solver.html'
-            },
-
-        ];
-
-        navItems.forEach((item) => {
-            const navItem = document.createElement('li');
-            navItem.className = 'nav-item';
-
-            const navLink = document.createElement('a');
-            navLink.className = 'nav-link';
-            navLink.href = item.href;
-            navLink.textContent = item.text;
-
-            navItem.appendChild(navLink);
-            navList.appendChild(navItem);
-        });
-
-        offcanvasHeader.appendChild(offcanvasTitle);
-        offcanvasHeader.appendChild(closeBtn);
-
-        offcanvasBody.appendChild(navList);
-
-        offcanvas.appendChild(offcanvasHeader);
-        offcanvas.appendChild(offcanvasBody);
-
-        containerFluid.appendChild(brand);
-        containerFluid.appendChild(toggler);
-        containerFluid.appendChild(offcanvas);
-
-        nav.appendChild(containerFluid);
-
-        this.container.appendChild(nav);
-    }
-}
-
-const navContainer = document.querySelector('.nav-container');
-new Nav(navContainer);
+const nav = document.querySelector(".nav-container");
+nav.innerHTML = `
+  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid"> <a class="navbar-brand" href="index.html">WT</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">WT</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item"> <a class="nav-link" aria-current="page" href="index.html">Day</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="week.html">Week</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="yearly.html">Weekly Totals</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="weight.html">Weight</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="Boosters.html">Energy</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="list.html">To-do</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="solver.html">Groceries</a> </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+`;
